4f887d8bbc3c649a80a9136a26f448812cf46a73
PosterMyWall <postermywall@Taimur-Hassans-MacBook-Pro.local>
2023-10-11 17:37:14 +0500

    Initial commit

---
+++ src/components/all-articles-page/all-articles.jsx
@@ -0,0 +1,37 @@
+import ArticlesGrid from "../articles-grid/articles-grid";
+import { useState, useEffect } from "react";
+
+function AllArticles() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Define the endpoint URL
+    const apiUrl = "http://localhost:7777/all";
+
+    // Make an HTTP GET request to the endpoint
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((result) => {
+        console.log(result);
+        // Update the state with the fetched data
+        setData(result);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setLoading(false);
+      });
+  }, []); // Empty dependency array ensures the effect runs once on component mount
+
+  return (
+    <ArticlesGrid articles={data}/>
+  );
+}
+
+export default AllArticles;
